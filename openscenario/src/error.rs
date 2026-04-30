d78d55ee6d57180ca17e2f5bdd7ed715e7a79b58
Jake <jeremiah.aboganda@gmail.com>
2026-04-30 09:47:40 +0000

    feat: add version enum and error types with tests

--- openscenario/src/error.rs
+++ openscenario/src/error.rs
@@ -1,11 +1,7 @@
-//! Error types for OpenSCENARIO library
-
 use thiserror::Error;
 
-/// Result type for OpenSCENARIO operations
 pub type Result<T> = std::result::Result<T, ScenarioError>;
 
-/// Errors that can occur during scenario operations
 #[derive(Debug, Error)]
 #[non_exhaustive]
 pub enum ScenarioError {
